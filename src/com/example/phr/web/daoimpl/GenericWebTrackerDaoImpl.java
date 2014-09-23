46bcdeab49da118f2aa05628f3a9f6a21b0822b9
givmefive5 <givme_five5@Yahoo.com>
2014-09-23 10:34:50 +0800

    s

---
+++ src/com/example/phr/web/daoimpl/GenericWebTrackerDaoImpl.java
@@ -0,0 +1,144 @@
+package com.example.phr.web.daoimpl;
+
+import java.util.List;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.example.phr.exceptions.OutdatedAccessTokenException;
+import com.example.phr.exceptions.WebServerException;
+import com.example.phr.tools.GSONConverter;
+
+public abstract class GenericWebTrackerDaoImpl<TrackerEntry> extends
+		BasicDaoImpl {
+
+	public int add_ReturnEntryIDToWebUsingHttp(String command,
+			TrackerEntry object) throws WebServerException,
+			OutdatedAccessTokenException {
+		try {
+			JSONObject data = new JSONObject();
+			data.put("accessToken", userDao.getAccessToken().getAccessToken());
+			data.put("objectToAdd", GSONConverter.convertObjectToJSON(object));
+			String jsonToSend = jsonRequestCreator
+					.createJSONRequest(data, null);
+			System.out.println("JSON Request Sent: " + jsonToSend);
+			JSONObject response = performHttpRequest_JSON(command, jsonToSend);
+			System.out.println("JSON Response Received: " + response);
+
+			if (response.getJSONObject("data").has("isValidAccessToken")
+					&& response.getJSONObject("data")
+							.getString("isValidAccessToken").equals("false")) {
+				throw new OutdatedAccessTokenException(
+						"The access token used in the request is outdated, please ask the user to log in again.");
+			} else if (response.get("status").equals("success")) {
+				int entryID = response.getJSONObject("data").getInt("entryID");
+				return entryID;
+			} else {
+				throw new WebServerException(
+						"An error has occurred while communicating"
+								+ "with the web server.");
+			}
+
+		} catch (JSONException e) {
+			throw new WebServerException(
+					"An error has occurred while communicating"
+							+ "with the web server.");
+		}
+	}
+
+	public void editUsingHttp(String command, TrackerEntry object)
+			throws WebServerException, OutdatedAccessTokenException {
+		try {
+			JSONObject data = new JSONObject();
+			data.put("accessToken", userDao.getAccessToken().getAccessToken());
+			data.put("objectToEdit", GSONConverter.convertObjectToJSON(object));
+			String jsonToSend = jsonRequestCreator
+					.createJSONRequest(data, null);
+			System.out.println("JSON Request Sent: " + jsonToSend);
+			JSONObject response = performHttpRequest_JSON(command, jsonToSend);
+			System.out.println("JSON Response Received: " + response);
+
+			if (response.getJSONObject("data").has("isValidAccessToken")
+					&& response.getJSONObject("data")
+							.getString("isValidAccessToken").equals("false")) {
+				throw new OutdatedAccessTokenException(
+						"The access token used in the request is outdated, please ask the user to log in again.");
+			} else if (response.get("status").equals("success")) {
+				System.out.println("Editing successful");
+
+			} else {
+				throw new WebServerException(
+						"An error has occurred while communicating"
+								+ "with the web server.");
+			}
+
+		} catch (JSONException e) {
+			throw new WebServerException(
+					"An error has occurred while communicating"
+							+ "with the web server.");
+		}
+	}
+
+	public void deleteUsingHttp(String command, TrackerEntry object)
+			throws WebServerException, OutdatedAccessTokenException {
+		try {
+			JSONObject data = new JSONObject();
+			data.put("accessToken", userDao.getAccessToken().getAccessToken());
+			data.put("objectToDelete",
+					GSONConverter.convertObjectToJSON(object));
+			String jsonToSend = jsonRequestCreator
+					.createJSONRequest(data, null);
+			System.out.println("JSON Request Sent: " + jsonToSend);
+			JSONObject response = performHttpRequest_JSON(command, jsonToSend);
+			System.out.println("JSON Response Received: " + response);
+
+			if (response.getJSONObject("data").has("isValidAccessToken")
+					&& response.getJSONObject("data")
+							.getString("isValidAccessToken").equals("false")) {
+				throw new OutdatedAccessTokenException(
+						"The access token used in the request is outdated, please ask the user to log in again.");
+			} else if (response.get("status").equals("success")) {
+				System.out.println("Deleting successful");
+			} else {
+				throw new WebServerException(
+						"An error has occurred while communicating"
+								+ "with the web server.");
+			}
+
+		} catch (JSONException e) {
+			throw new WebServerException(
+					"An error has occurred while communicating"
+							+ "with the web server.");
+		}
+	}
+
+	public List<TrackerEntry> getAllUsingHttp(String command,
+			Class<TrackerEntry> classTypeToGenerate) throws WebServerException,
+			OutdatedAccessTokenException {
+		try {
+			JSONObject data = new JSONObject();
+			data.put("accessToken", userDao.getAccessToken().getAccessToken());
+			String jsonToSend = jsonRequestCreator
+					.createJSONRequest(data, null);
+			System.out.println("JSON Request Sent: " + jsonToSend);
+			JSONObject response = performHttpRequest_JSON(command, jsonToSend);
+			System.out.println("JSON Response Received: " + response);
+			if (response.getJSONObject("data").has("isValidAccessToken")
+					&& response.getJSONObject("data")
+							.getString("isValidAccessToken").equals("false")) {
+				throw new OutdatedAccessTokenException(
+						"The access token used in the request is outdated, please ask the user to log in again.");
+			} else if (response.get("status").equals("success")) {
+				String list = response.getJSONObject("data").getString("list");
+				return GSONConverter.convertJSONToObjectList(list,
+						classTypeToGenerate);
+			} else {
+				throw new WebServerException(
+						"An error has occurred while communicating"
+								+ "with the web server.");
+			}
+		} catch (JSONException e) {
+			throw new WebServerException("Error has occurred", e);
+		}
+	}
+}
