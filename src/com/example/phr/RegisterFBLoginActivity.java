e3b95d959e33f65247f664d188205295b739537d
ImTheMark <mark_serapion@yahoo.com>
2014-10-07 21:44:22 +0800

    FB Login Implementation [NOT TESTED]

--- src/com/example/phr/RegisterFBLoginActivity.java
+++ src/com/example/phr/RegisterFBLoginActivity.java
@@ -1,21 +1,146 @@
 package com.example.phr;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
+import com.facebook.model.GraphUser;
 import com.facebook.widget.LoginButton;
+import com.facebook.widget.LoginButton.UserInfoChangedCallback;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class RegisterFBLoginActivity extends Activity {
 	
-	LoginButton btnLogin;
+	private LoginButton btnLogin;
+	private UiLifecycleHelper uiHelper;
+	private static final List<String> PERMISSIONS = Arrays.asList("user_birthday", "email", "read_stream", "publish_actions");
+	private TextView userName;
+	public static GraphUser user;
+	public static String userID;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		uiHelper = new UiLifecycleHelper(this, statusCallback);
+		uiHelper.onCreate(savedInstanceState);
+		
 		setContentView(R.layout.activity_registration_fblogin);
+		
+		userName = (TextView) findViewById(R.id.textViewRegistrationFBTitle);
+		btnLogin = (LoginButton) findViewById(R.id.fb_login_button);
+		btnLogin.setUserInfoChangedCallback(new UserInfoChangedCallback() {
+			@Override
+			public void onUserInfoFetched(GraphUser user) {
+				RegisterFBLoginActivity.this.user = user;
+				if (user != null) {
+					userName.setText("Hello, " + user.getName());
+					userID = user.getUsername();
+					//new LoadFbProfilePicture(profilePictureView).execute(user.getId());
+				} else {
+					userName.setText("You are not logged");
+				}
+			}
+		});
+		
+	}
+	
+	private Session.StatusCallback statusCallback = new Session.StatusCallback() {
+		@Override
+		public void call(Session session, SessionState state,
+				Exception exception) {
+			if (state.isOpened()) {
+				
+				Log.d("FB", "Facebook session opened");
+				
+				
+				try {
+					facebookRequest(user.getId(), session.getAccessToken());
+				} catch (ClientProtocolException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				
+			} else if (state.isClosed()) {
+				Log.d("FB", "Facebook session closed");
+			}
+		}
+	};
+
+	public boolean checkPermissions() {
+		Session s = Session.getActiveSession();
+		if (s != null) {
+			return s.getPermissions().contains(Arrays.asList("user_birthday", "email", "read_stream", "publish_actions"));
+		} else
+			return false;
+	}
+
+	public void requestPermissions() {
+		Session s = Session.getActiveSession();
+		if (s != null)
+			s.requestNewPublishPermissions(new Session.NewPermissionsRequest(
+					this, PERMISSIONS));
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		uiHelper.onResume();
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		uiHelper.onPause();
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		uiHelper.onDestroy();
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		uiHelper.onActivityResult(requestCode, resultCode, data);
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle savedState) {
+		super.onSaveInstanceState(savedState);
+		uiHelper.onSaveInstanceState(savedState);
 	}
 
 	@Override
@@ -35,5 +160,57 @@ public class RegisterFBLoginActivity extends Activity {
 			return super.onOptionsItemSelected(item);
 		}
 
+	}
+
+	// Request to our java server.
+	private void facebookRequest(String userId, String token)
+			throws ClientProtocolException, IOException {
+		String address = "http://10.0.2.2:8080/JSONTestServer/FacebookRequestServlet";
+		HttpClient client = new DefaultHttpClient();
+		HttpPost post = new HttpPost(address);
+
+		List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+		nameValuePairs.add(new BasicNameValuePair("userId", userId));
+		nameValuePairs.add(new BasicNameValuePair("accessToken", token));
+		post.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+		HttpResponse response = client.execute(post);
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		response.getEntity().writeTo(out);
+		out.close();
+		String responseString = out.toString();
+		System.out.println(responseString);
+	}
+	
+	private class LoadFbProfilePicture extends AsyncTask<String, Void, Bitmap> {
+	    ImageView bmImage;
+	 
+	    public LoadFbProfilePicture(ImageView bmImage) {
+	        this.bmImage = bmImage;
+	    }
+	 
+	    protected Bitmap doInBackground(String... urls) {
+	        URL img_value = null;
+	        Log.e("LoginAct",urls[0]);
+			 try {
+				img_value = new URL("https://graph.facebook.com/"+urls[0]+"/picture?type=large");
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			Bitmap mIcon1 = null;
+			try {
+				mIcon1 = BitmapFactory.decodeStream(img_value.openConnection().getInputStream());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	        
+	        return mIcon1;
+	    }
+	 
+	    protected void onPostExecute(Bitmap result) {
+	        bmImage.setImageBitmap(result);
+	    }
 	}
 }
