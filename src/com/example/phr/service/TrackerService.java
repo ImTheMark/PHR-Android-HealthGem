252c7994d7d899149d6029f9ed85f6791b38a7f9
cathy888 <yung.c2468@gmail.com>
2014-10-13 15:11:02 +0800

    updated

--- src/com/example/phr/service/TrackerService.java
+++ src/com/example/phr/service/TrackerService.java
@@ -2,11 +2,9 @@ package com.example.phr.service;
 
 import java.util.List;
 
-import com.example.phr.exceptions.DataAccessException;
 import com.example.phr.exceptions.EntryNotFoundException;
 import com.example.phr.exceptions.OutdatedAccessTokenException;
 import com.example.phr.exceptions.ServiceException;
-import com.example.phr.exceptions.WebServerException;
 
 public interface TrackerService<T> {
 
@@ -16,10 +14,12 @@ public interface TrackerService<T> {
 	public void edit(T object) throws OutdatedAccessTokenException,
 			EntryNotFoundException, ServiceException;
 
-	public void delete(T object) throws OutdatedAccessTokenException, 
+	public void delete(T object) throws OutdatedAccessTokenException,
 			EntryNotFoundException, ServiceException;
 
 	public List<T> getAll() throws ServiceException;
 
 	public T get(int entryID);
+
+	public T getLatest();
 }
