95d33b4af01a27f9a1933b68cc5dec9dfe6c480d
ImTheMark <mark_serapion@yahoo.com>
2014-10-18 21:31:57 +0800

    FoodTrackerServiceImpl

--- src/com/example/phr/serviceimpl/FoodTrackerServiceImpl.java
+++ src/com/example/phr/serviceimpl/FoodTrackerServiceImpl.java
@@ -6,40 +6,74 @@ import com.example.phr.exceptions.DataAccessException;
 import com.example.phr.exceptions.EntryNotFoundException;
 import com.example.phr.exceptions.OutdatedAccessTokenException;
 import com.example.phr.exceptions.ServiceException;
+import com.example.phr.exceptions.WebServerException;
 import com.example.phr.mobile.dao.MobileFoodTrackerDao;
 import com.example.phr.mobile.daoimpl.MobileFoodTrackerDaoImpl;
 import com.example.phr.mobile.models.FoodTrackerEntry;
 import com.example.phr.service.FoodTrackerService;
+import com.example.phr.web.dao.WebFoodTrackerDao;
+import com.example.phr.web.daoimpl.WebFoodTrackerDaoImpl;
 
 public class FoodTrackerServiceImpl implements FoodTrackerService {
 
-	MobileFoodTrackerDao mobileFoodTrackerDao = new MobileFoodTrackerDaoImpl();
+	WebFoodTrackerDao webFoodTrackerDao;
+	MobileFoodTrackerDao mobileFoodTrackerDao;
+	
+	public FoodTrackerServiceImpl(){
+		webFoodTrackerDao = new WebFoodTrackerDaoImpl();
+		mobileFoodTrackerDao = new MobileFoodTrackerDaoImpl();
+	}
 
 	@Override
-	public void add(FoodTrackerEntry object) throws ServiceException,
+	public void add(FoodTrackerEntry foodTrackerEntry) throws ServiceException,
 			OutdatedAccessTokenException {
-		// TODO Auto-generated method stub
+		try {
+			webFoodTrackerDao.add_ReturnEntryIdInWeb(foodTrackerEntry);
+			mobileFoodTrackerDao.add(foodTrackerEntry);
+		} catch (WebServerException e) {
+			e.printStackTrace();
+		} catch (DataAccessException e) {
+			e.printStackTrace();
+		}
 
 	}
 
 	@Override
-	public void edit(FoodTrackerEntry object) throws ServiceException,
+	public void edit(FoodTrackerEntry foodTrackerEntry) throws ServiceException,
 			OutdatedAccessTokenException, EntryNotFoundException {
-		// TODO Auto-generated method stub
-
+		try {
+			webFoodTrackerDao.edit(foodTrackerEntry);
+			mobileFoodTrackerDao.edit(foodTrackerEntry);
+		} catch (WebServerException e) {
+			e.printStackTrace();
+		} catch (DataAccessException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
-	public void delete(FoodTrackerEntry object) throws ServiceException,
+	public void delete(FoodTrackerEntry foodTrackerEntry) throws ServiceException,
 			OutdatedAccessTokenException {
-		// TODO Auto-generated method stub
-
+		try {
+			webFoodTrackerDao.delete(foodTrackerEntry);
+			mobileFoodTrackerDao.delete(foodTrackerEntry);
+		} catch (WebServerException e) {
+			e.printStackTrace();
+		} catch (DataAccessException e) {
+			e.printStackTrace();
+		} catch (EntryNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
-	public List<FoodTrackerEntry> getAll() {
-		// TODO Auto-generated method stub
-		return null;
+	public List<FoodTrackerEntry> getAll() throws ServiceException {
+		try {
+			return mobileFoodTrackerDao.getAll();
+		} catch (DataAccessException e) {
+			throw new ServiceException(
+					"An error occured while trying to add cu to web", e);
+		}
 	}
 
 	@Override
