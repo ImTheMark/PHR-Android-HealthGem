b08b9d77f7b8afa7a94f491de3b06ccf856fcc23
ImTheMark <mark_serapion@yahoo.com>
2014-10-22 22:19:21 +0800

    getGroupedByDate for food and activity

--- src/com/example/phr/mobile/dao/MobileFoodTrackerDao.java
+++ src/com/example/phr/mobile/dao/MobileFoodTrackerDao.java
@@ -1,8 +1,13 @@
 package com.example.phr.mobile.dao;
 
+import java.util.List;
+
+import com.example.phr.exceptions.DataAccessException;
 import com.example.phr.mobile.models.FoodTrackerEntry;
 
 public interface MobileFoodTrackerDao extends
 		MobileTrackerDao<FoodTrackerEntry> {
+
+	public List<List<FoodTrackerEntry>> getAllGroupedByDate() throws DataAccessException;
 
 }
