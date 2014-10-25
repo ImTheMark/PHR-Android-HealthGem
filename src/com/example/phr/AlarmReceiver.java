5a560165028a088aeb820a305d6134b0cbc7767c
cathy888 <yung.c2468@gmail.com>
2014-10-25 14:04:18 +0800

    update

--- src/com/example/phr/AlarmReceiver.java
+++ src/com/example/phr/AlarmReceiver.java
@@ -1,7 +1,12 @@
 package com.example.phr;
 
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 import android.app.Notification;
 import android.app.NotificationManager;
@@ -13,40 +18,56 @@ import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.example.phr.exceptions.ServiceException;
+import com.example.phr.mobile.models.BloodSugar;
 import com.example.phr.serviceimpl.BloodSugarTrackerServiceImpl;
+import com.example.phr.tools.DateTimeParser;
 
 public class AlarmReceiver extends BroadcastReceiver {
 
 	private static final int MY_NOTIFICATION_ID = 1;
 	NotificationManager notificationManager;
 	Notification myNotification;
+	DateFormat dateFormat;
+	DateFormat timeFormat;
+	DateFormat fmt;
+	Calendar calobj;
+	Intent myIntent;
+	PendingIntent pendingIntent;
+	String title;
+	String content;
+	String ticker;
+	Context thisContext;
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
 
-		Intent myIntent = new Intent(context, MainActivity.class);
-		PendingIntent pendingIntent = PendingIntent.getActivity(context, 0,
-				myIntent, Intent.FLAG_ACTIVITY_NEW_TASK);
-		String title = "HealthGem";
-		String content = "what did you eat?";
-		String ticker = "What did you eat?";
-		SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+		myIntent = new Intent(context, MainActivity.class);
+		pendingIntent = PendingIntent.getActivity(context, 0, myIntent,
+				Intent.FLAG_ACTIVITY_NEW_TASK);
+		thisContext = context;
 
-		BloodSugarTrackerServiceImpl bsService = new BloodSugarTrackerServiceImpl();
-		Log.e("bsservice", "called");
-		Date lastBs = null;
-		Date currentDate = new Date();
-
+		// get current time
+		dateFormat = new SimpleDateFormat("MMM dd, yyyy", Locale.ENGLISH);
+		timeFormat = new SimpleDateFormat("HH:mm:ss", Locale.ENGLISH);
+		fmt = new SimpleDateFormat("MMM dd, yyyy HH:mm:ss", Locale.ENGLISH);
+		calobj = Calendar.getInstance();
+		Date date = null;
 		try {
-			lastBs = new Date(bsService.getLatest().getTimestamp().getTime());
-		} catch (ServiceException e) {
+			date = fmt.parse(dateFormat.format(calobj.getTime()) + " "
+					+ timeFormat.format(calobj.getTime()));
+		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		Timestamp timestamp = new Timestamp(date.getTime());
 
-		if (lastBs != null)
-			Log.e("bsLast", format.format(lastBs));
-		Log.e("currentDate", format.format(currentDate));
+		// check condition
+		if (showBsNotif(timestamp)) {
+			title = "HealthGem";
+			content = "What's your sugar level?";
+			ticker = "It's time to measure sugar level!";
+			showNotification();
+		}
 
 		/*
 		 * Date d1 = null; Date d2 = null;
@@ -66,14 +87,41 @@ public class AlarmReceiver extends BroadcastReceiver {
 		 * } catch (Exception e) { e.printStackTrace(); }
 		 */
 
-		myNotification = new NotificationCompat.Builder(context)
+	}
+
+	public boolean showBsNotif(Timestamp current) {
+		boolean notif = true; // show notif
+
+		BloodSugarTrackerServiceImpl bsService = new BloodSugarTrackerServiceImpl();
+		Log.e("bsservice", "called");
+		BloodSugar lastBs = null;
+		try {
+			lastBs = bsService.getLatest();
+		} catch (ServiceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// dont show notif
+		if (lastBs != null
+				&& String.valueOf(
+						DateTimeParser.getMonth(lastBs.getTimestamp())).equals(
+						String.valueOf(DateTimeParser.getMonth(current)))
+				&& String.valueOf(DateTimeParser.getDay(lastBs.getTimestamp()))
+						.equals(String.valueOf(DateTimeParser.getDay(current))))
+			notif = false;
+
+		return notif;
+	}
+
+	public void showNotification() {
+		myNotification = new NotificationCompat.Builder(thisContext)
 				.setContentTitle(title).setContentText(content)
 				.setTicker(ticker).setWhen(System.currentTimeMillis())
 				.setContentIntent(pendingIntent)
 				.setDefaults(Notification.DEFAULT_SOUND).setAutoCancel(true)
 				.setSmallIcon(R.drawable.heart_gem).build();
 
-		notificationManager = (NotificationManager) context
+		notificationManager = (NotificationManager) thisContext
 				.getSystemService(Context.NOTIFICATION_SERVICE);
 		notificationManager.notify(MY_NOTIFICATION_ID, myNotification);
 	}
