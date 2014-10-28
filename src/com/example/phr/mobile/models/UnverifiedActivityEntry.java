093a59d10b8a4f91474c1fac865d994393dd26c3
givmefive5 <givme_five5@Yahoo.com>
2014-10-28 20:05:33 +0800

    unverifiedactentry

--- src/com/example/phr/mobile/models/UnverifiedActivityEntry.java
+++ src/com/example/phr/mobile/models/UnverifiedActivityEntry.java
@@ -4,34 +4,39 @@ import java.sql.Timestamp;
 
 public class UnverifiedActivityEntry extends TrackerEntry {
 
-	String activityName;
+	ActivitySingle activity;
 	int durationInSeconds;
-	int calorieBurnedPerHour;
+	Double calorieBurnedPerHour;
+	String extractedWord;
 
 	public UnverifiedActivityEntry(Integer entryID, String facebookID,
 			Timestamp timestamp, String status, PHRImage image,
-			String activityName, int durationInSeconds, int calorieBurnedPerHour) {
+			ActivitySingle activity, int durationInSeconds,
+			Double calorieBurnedPerHour, String extractedWord) {
 		super(entryID, facebookID, timestamp, status, image);
-		this.activityName = activityName;
+		this.activity = activity;
 		this.durationInSeconds = durationInSeconds;
 		this.calorieBurnedPerHour = calorieBurnedPerHour;
+		this.extractedWord = extractedWord;
 	}
 
 	public UnverifiedActivityEntry(String facebookID, Timestamp timestamp,
-			String status, PHRImage image, String activityName,
-			int durationInSeconds, int calorieBurnedPerHour) {
+			String status, PHRImage image, ActivitySingle activity,
+			int durationInSeconds, Double calorieBurnedPerHour,
+			String extractedWord) {
 		super(facebookID, timestamp, status, image);
-		this.activityName = activityName;
+		this.activity = activity;
 		this.durationInSeconds = durationInSeconds;
 		this.calorieBurnedPerHour = calorieBurnedPerHour;
+		this.extractedWord = extractedWord;
 	}
 
-	public String getActivityName() {
-		return activityName;
+	public ActivitySingle getActivity() {
+		return activity;
 	}
 
-	public void setActivityName(String activityName) {
-		this.activityName = activityName;
+	public void setActivity(ActivitySingle activity) {
+		this.activity = activity;
 	}
 
 	public int getDurationInSeconds() {
@@ -42,12 +47,20 @@ public class UnverifiedActivityEntry extends TrackerEntry {
 		this.durationInSeconds = durationInSeconds;
 	}
 
-	public int getCalorieBurnedPerHour() {
+	public Double getCalorieBurnedPerHour() {
 		return calorieBurnedPerHour;
 	}
 
-	public void setCalorieBurnedPerHour(int calorieBurnedPerHour) {
+	public void setCalorieBurnedPerHour(Double calorieBurnedPerHour) {
 		this.calorieBurnedPerHour = calorieBurnedPerHour;
+	}
+
+	public String getExtractedWord() {
+		return extractedWord;
+	}
+
+	public void setExtractedWord(String extractedWord) {
+		this.extractedWord = extractedWord;
 	}
 
 }
