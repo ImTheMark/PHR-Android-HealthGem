eb6c19d90432fd2ec51f2f28c92ffbe88f131ed9
ImTheMark <mark_serapion@yahoo.com>
2014-11-05 21:34:28 +0800

    no back on SplashScreen

--- src/com/example/phr/SplashActivity.java
+++ src/com/example/phr/SplashActivity.java
@@ -31,10 +31,14 @@ public class SplashActivity extends Activity {
 						.getAccessToken() != null) {
 					Intent intent = new Intent(getApplicationContext(),
 							MainActivity.class);
+					intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+							| Intent.FLAG_ACTIVITY_CLEAR_TOP);
 					startActivity(intent);
 				} else {
 					Intent intent = new Intent(getApplicationContext(),
 							LoginActivity.class);
+					intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+							| Intent.FLAG_ACTIVITY_CLEAR_TOP);
 					startActivity(intent);
 				}
 			}
