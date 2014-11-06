eab96210dfe3f79f2a9a0fe228866e8995ac16f9
cathy888 <yung.c2468@gmail.com>
2014-11-06 14:17:36 +0800

    add error message in exceptions

--- src/com/example/phr/exceptions/ImageHandlerException.java
+++ src/com/example/phr/exceptions/ImageHandlerException.java
@@ -1,9 +1,15 @@
 package com.example.phr.exceptions;
 
+import android.widget.Toast;
+
+import com.example.phr.application.HealthGem;
+
 public class ImageHandlerException extends Exception {
 
 	public ImageHandlerException(String message, Exception e) {
 		super(message, e);
+		Toast.makeText(HealthGem.getContext(), "Error!", Toast.LENGTH_LONG)
+				.show();
 	}
 
 }
--- src/com/example/phr/exceptions/WebServerException.java
+++ src/com/example/phr/exceptions/WebServerException.java
@@ -1,9 +1,15 @@
 package com.example.phr.exceptions;
 
+import android.widget.Toast;
+
+import com.example.phr.application.HealthGem;
+
 public class WebServerException extends Exception {
 
 	public WebServerException(String message, Exception e) {
 		super(message, e);
+		Toast.makeText(HealthGem.getContext(), "No Internet Connection !",
+				Toast.LENGTH_LONG).show();
 	}
 
 	public WebServerException(String message) {
