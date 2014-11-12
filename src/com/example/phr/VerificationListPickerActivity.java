7a53f7f7edf6a2501939b349c6cb321f220448e2
ImTheMark <mark_serapion@yahoo.com>
2014-11-12 19:49:42 +0800

    Verification activity list from web with asy

--- src/com/example/phr/VerificationListPickerActivity.java
+++ src/com/example/phr/VerificationListPickerActivity.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.util.Log;
@@ -13,14 +14,19 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.phr.adapter.SingleFoodAdapter;
+import com.example.phr.adapter.UnverifiedStatusAdapter;
+import com.example.phr.application.HealthGem;
 import com.example.phr.enums.TrackerInputType;
+import com.example.phr.exceptions.OutdatedAccessTokenException;
 import com.example.phr.exceptions.ServiceException;
 import com.example.phr.mobile.models.Activity;
 import com.example.phr.mobile.models.ActivityTrackerEntry;
 import com.example.phr.mobile.models.Food;
 import com.example.phr.mobile.models.FoodTrackerEntry;
+import com.example.phr.mobile.models.TrackerEntry;
 import com.example.phr.mobile.models.UnverifiedRestaurantEntry;
 import com.example.phr.mobile.models.UnverifiedSportsEstablishmentEntry;
 import com.example.phr.mobile.models.Weight;
@@ -88,51 +94,90 @@ public class VerificationListPickerActivity extends android.app.Activity {
 		} else if (extras != null && in.hasExtra("sportestablishment")) {
 			sportsEntry = (UnverifiedSportsEstablishmentEntry) in.getExtras()
 					.getSerializable("sportestablishment");
+			new GetSportEstablishmentPostFromServer().execute();
+		}
+	}
 
-			activityList = verificationService.getActivityListGivenEstablishmentID(sportsEntry.getSportEstablishment().getEntryID());
-			List<String> actListNameOnly = new ArrayList<String>();
+	class GetSportEstablishmentPostFromServer extends AsyncTask<Void, Void, Void> {
+		
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			
+			Log.e("VERIFICATION ASYNC", "async start");
+		}
 
-			for (Activity act : activityList)
-				actListNameOnly.add(act.getName());
+		@Override
+		protected Void doInBackground(Void... params) {
+			UnverifiedSportsEstablishmentEntry entry;
+			try {
 
-			ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-					getApplicationContext(), R.layout.item_custom_listview,
-					actListNameOnly);
+				
+				Log.e("VERIFICATION ASYNC", "pulling from web");
+				
+				entry = verificationService.getUnverifiedSportsEstablishmentPostFromWebDB(sportsEntry);
 
-			listView.setAdapter(adapter);
+				List<String> actListNameOnly = new ArrayList<String>();
 
-			listView.setOnItemClickListener(new OnItemClickListener() {
+				for (Activity act : entry.getActivities())
+					actListNameOnly.add(act.getName());
 
-				@Override
-				public void onItemClick(AdapterView<?> arg0, View arg1,
-						int arg2, long arg3) {
+				ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+						getApplicationContext(), R.layout.item_custom_listview,
+						actListNameOnly);
+				
 
-					Activity chosenActivity = activityList.get(arg2);
-					Weight weight = null;
-					WeightTrackerService weightService = new WeightTrackerServiceImpl();
-					try {
-						weight = weightService.getLatest();
-					} catch (ServiceException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
+				listView.setAdapter(adapter);
+
+				listView.setOnItemClickListener(new OnItemClickListener() {
+
+					@Override
+					public void onItemClick(AdapterView<?> arg0, View arg1,
+							int arg2, long arg3) {
+
+						Activity chosenActivity = activityList.get(arg2);
+						Weight weight = null;
+						WeightTrackerService weightService = new WeightTrackerServiceImpl();
+						try {
+							weight = weightService.getLatest();
+						} catch (ServiceException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						double cal = chosenActivity.getMET()
+								* weight.getWeightInKilograms() * 1;
+
+						ActivityTrackerEntry actEntry = new ActivityTrackerEntry(
+								sportsEntry.getEntryID(), sportsEntry
+										.getFacebookID(), sportsEntry
+										.getTimestamp(), sportsEntry.getStatus(),
+								sportsEntry.getImage(), chosenActivity, cal, 3600);
+
+						Intent i = new Intent(getApplicationContext(),
+								NewStatusActivity.class);
+						i.putExtra("unverified", TrackerInputType.SPORTS);
+						i.putExtra("object", sportsEntry);
+						i.putExtra("object1", actEntry);
+						startActivity(i);
 					}
-					double cal = chosenActivity.getMET()
-							* weight.getWeightInKilograms() * 1;
+				});
+				
+			} catch (ServiceException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (OutdatedAccessTokenException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
 
-					ActivityTrackerEntry actEntry = new ActivityTrackerEntry(
-							sportsEntry.getEntryID(), sportsEntry
-									.getFacebookID(), sportsEntry
-									.getTimestamp(), sportsEntry.getStatus(),
-							sportsEntry.getImage(), chosenActivity, cal, 3600);
+			return null;
+		}
 
-					Intent i = new Intent(getApplicationContext(),
-							NewStatusActivity.class);
-					i.putExtra("unverified", TrackerInputType.SPORTS);
-					i.putExtra("object", sportsEntry);
-					i.putExtra("object1", actEntry);
-					startActivity(i);
-				}
-			});
+		@Override
+		protected void onPostExecute(Void result) {
+			super.onPostExecute(result);
+			
+			Log.e("VERIFICATION ASYNC", "done");
 		}
 	}
 
